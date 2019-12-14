3c946429840338482348c8b79e321c74466dbf3d
WHOamIworld <ddreytblat@gmail.com>
2019-12-13 22:11:34 -0500

    aftercode

---
+++ Something.py
@@ -0,0 +1,15 @@
+print("Hello World")
+foo=14
+print(foo)
+str="160 princeton oval"
+print(str)
+list=["abby", "danny", "nestor", "sasha", "ben"]
+print(list[0])
+print(list[2:5])
+print(list[1])
+dict={"danny":14, "abby":15, "nestor":16, "sasha":16, "ben":59}
+print(dict["danny"])
+print(dict["abby"])
+print(dict["nestor"])
+print(dict["sasha"])
+print(dict["ben"])
