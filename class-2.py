1414d78e354e8b83646194da36180cd1d5fe0a76
WHOamIworld <ddreytblat@gmail.com>
2019-12-27 21:25:39 -0500

    com

--- class-2.py
+++ class-2.py
@@ -1,6 +1,7 @@
 def foo(z, x, y):
     W = z*x + y
-    print("w is %d" %w)
-    return w(z, x, y)
+    print("w is %d" % w)
+    print("args=%dx %dy %dz" %)
+    return w
 r=foo(1, 7, 10)
 print(r)
---
+++ class2pt2.py
@@ -0,0 +1,9 @@
+x=input('plstypeinefirstarg')
+y=input('plstypeinsecondarg')
+if x==y:
+    print('yes')
+elif x=='hi':
+    print('mebbe')
+else:
+    print('no')
+    
