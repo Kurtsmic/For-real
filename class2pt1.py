b51d858b0bad0a4988a0b97eefcec43bcc65a2d3
WHOamIworld <ddreytblat@gmail.com>
2019-12-27 21:49:02 -0500

    kermit

--- class2pt1.py
+++ class2pt1.py
@@ -1,7 +1,7 @@
 def foo(z, x, y):
-    W = z*x + y
+    w = z*x + y
     print("w is %d" % w)
-    print("args=%dx %dy %dz" %)
+    print("args=%d %d %d" % (x,y,z))
     return w
 r=foo(1, 7, 10)
 print(r)
---
+++ class2pt3.py
@@ -0,0 +1,5 @@
+in1=input('plstypeingnumber')
+i=int(in1)
+while(i>0):
+    print('i=%d' %16 i)
+    i=i-1
