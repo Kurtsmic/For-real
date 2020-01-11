212fce12369e3012b57bb8a9f79181f927b0e83a
WHOamIworld <ddreytblat@gmail.com>
2020-01-10 23:43:38 -0500

    com

--- practice14.py
+++ practice14.py
@@ -1,7 +1,7 @@
-import datetime 
+import datetime as DT
 x = input('day first :')
 y = input('day second:')
-dx = datetime.datetime.strptime(x, '%Y-%m-%d')
-dy = datetime.datetime.strptime(y, '%Y-%m-%d')
+dx = DT.datetime.strptime(x, '%Y-%m-%d')
+dy = DT.datetime.strptime(y, '%Y-%m-%d')
 delta = dy - dx
 print(delta)
---
+++ practice19.py
@@ -0,0 +1,3 @@
+x = str(input('say something :'))
+if len(str) >= 2 and str[:2] == "Is":
+    print(Is + x)
