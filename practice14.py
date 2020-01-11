4cee89cb8302f7c621d04968a58528ae062066a4
WHOamIworld <ddreytblat@gmail.com>
2020-01-10 22:05:08 -0500

    com

--- practice14.py
+++ practice14.py
@@ -1,5 +1,7 @@
 from datetime import date
 x = int(input('day first :'))
 y = int(input('day second:'))
-delta = date(y - x)
+datetime(x, '%Y, %m, %d')
+datetime(y, '%Y, %m, %d')
+delta = (y - x)
 print(delta)
---
+++ practice17.py
@@ -0,0 +1,9 @@
+x = int(input('numnum :'))
+delta1 = abs(1000-x)
+if delta1<=100:
+    print("true")
+delta2 = abs(2000-x)
+if delta2<=100:
+    print("also true")
+else:
+     print('false')
